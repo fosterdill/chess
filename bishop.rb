6e100fdeff6da9b964481fbdc478aea23538bdbd
App Academy Student <student@appacademy.io>
2013-11-26 14:29:53 -0800

    add #move_in_direction, #moves to SlidingPiece

--- bishop.rb
+++ bishop.rb
@@ -1,1 +1,6 @@
 require_relative 'sliding_piece'
+class Bishop
+  def move_directions
+    MOVE_DIRECTIONS[:diagonals]
+  end
+end
--- sliding_piece.rb
+++ sliding_piece.rb
@@ -17,7 +17,25 @@ class SlidingPiece < Piece
     ]
   }
 
-  def moves
+  def move_directions
+  end
 
+  def moves
+    moves = []
+    move_directions.each do |direction|
+      moves += move_in_direction(self.position, direction)
+    end
+
+    moves
+  end
+
+
+  end
+
+  def move_in_direction(pos, direction)
+    return [] if !in_bounds?(pos) || on_piece(pos)
+
+    new_pos = [pos[0] + direction[0], pos[1] + direction[1]]
+    [pos] + move_in_direction(new_pos, direction)
   end
 end
